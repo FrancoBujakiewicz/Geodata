8bd6d6fe3d36c33d432d9eecb0d406b5a6eec1d0
FrancoBujakiewicz <francobujakiewicz@gmail.com>
2026-06-30 11:14:49 -0300

    feat: Simplififcando persistencia, registrando gmail directamente en cada registro.

--- gas/Persistencia.js
+++ gas/Persistencia.js
@@ -1,55 +1,42 @@
 
- const SHEET_ID = PropertiesService.getScriptProperties().getProperty('SHEET_ID');
- const sheet = SpreadsheetApp.openById(SHEET_ID).getActiveSheet();
+const SHEET_ID = PropertiesService.getScriptProperties().getProperty('SHEET_ID');
+const sheet = SpreadsheetApp.openById(SHEET_ID).getActiveSheet();
 
- const columnaId = 11;
- const rangoCiudadano = sheet.getRange(1, 1, 1, 10);
- const rangoUsuario = sheet.getRange(1, columnaId, 1, 2);
+const rangoCiudadano = sheet.getRange(1, 1, 1, 10);
 
- persistenciaRegistrar(datosCiudadano, gmailRegistrador) {
+function persistenciaRegistrar(datosCiudadano, gmailRegistrador) {
+  sheet.appendRow([
+    datosCiudadano.dni,
+    datosCiudadano.nombres,
+    datosCiudadano.apellido,
+    datosCiudadano.numWhatsapp,
+    datosCiudadano.longitud,
+    datosCiudadano.latitud,
+    datosCiudadano.direccion,
+    datosCiudadano.observacion,
+    false, // estaEliminado
+    gmailRegistrador
+  ]);
 
+  return datosCiudadano;
+}
 
- }
+function persistenciaBuscar(dni) {}
+function persistenciaEditar(datosCiudadano) {}
+function persistenciaEliminar(dni) {}
 
- persistenciaBuscar() {}
- persistenciaEditar() {}
- persistenciaEliminar() {}
+function inicializarSheet() {
 
- function inicializarSheet() {
- 
-   sheet.clear();
- 
-   sheet.appendRow([
-    'dni', 'nombres', 'apellido', 'num_whatsapp', 
-    'longitud', 'latitud', 'direccion', 'observacion', 
-    'estaEliminado', 'registrador', 'registradorId', 'gmail'
-   ]);
+  sheet.clear(); // DEJA LA SHEET EN BLANCO!
+
+  sheet.appendRow([
+    'dni', 'nombres', 'apellido', 'num_whatsapp',
+    'longitud', 'latitud', 'direccion', 'observacion',
+    'estaEliminado', 'registrador'
+  ]);
 
   rangoCiudadano.setFontWeight('bold');
   rangoCiudadano.setBackground('#4a86e8');
   rangoCiudadano.setHorizontalAlignment('center');
   rangoCiudadano.setWrapStrategy(SpreadsheetApp.WrapStrategy.CLIP);
-
-  rangoUsuario.setFontWeight('bold');
-  rangoUsuario.setBackground('#6aa84f');
-  rangoUsuario.setHorizontalAlignment('center');
-  rangoUsuario.setWrapStrategy(SpreadsheetApp.WrapStrategy.CLIP);
- 
-  const CANTIDAD_FILAS = 50;
-
-  for (let i = 0; i < CANTIDAD_FILAS; i++) {
-
-   const filaActual = 2 + i;
-   const filaAnterior = filaActual - 1;
-
-   // Mapeo del número de fila a código ASCII
-   const letraId = String.fromCharCode(64 + columnaId);
-   const letraGmail = String.fromCharCode(64 + columnaId + 1);
-
-   const formula = `=IF(${letraGmail}${filaActual}="";"";IF(ISNUMBER(${letraId}${filaAnterior});${letraId}${filaAnterior}+1;1))`;
-
-   sheet.getRange(filaActual, columnaId).setFormula(formula);
-
- }
-
 }
